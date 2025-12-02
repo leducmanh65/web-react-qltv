4c138e7c1310a80536b19d2a4b2104b443931af2
unknown <lducmanh65@gmail.com>
2025-12-02 19:33:47 +0700

    Login

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -1,41 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/LoginSignup.css'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/padlock.png'
+import phone_icon from '../Assets/icons8-phone-50.png'
+import date_icon from '../Assets/icons8-date-50.png'
+import location_icon from '../Assets/icons8-location-24.png'
 import user_icon from '../Assets/user.png'
 
+const LoginSignup = () => {
+  const [action, setAction] = useState("Login");
 
-function LoginSignup() {
-    return (
-        <div className = 'container'>
-            <div class="header">
-                <div class="text">Sign Up</div>
-                <div class="underline"></div>
-            </div>
-            <div class="inputs">
-                <div class="input">
-                    <img src={user_icon} alt=""/>
-                    <input type="text" placeholder="Name" />
-                </div>
-                <div class="input">
-                    <img src={email_icon} alt=""/>
-                    <input type="email" placeholder="Email" />
-                </div>
-                <div class="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder="Password" />
-                </div>
-                <div class="forgot-password">Lost Password <span>Click here</span></div>
-                <div class="submit-container">
-                    <div class="submit">Sign Up</div>
-                    <div class="submit">Login</div>
-                </div>
-            </div>
-            
+  return (
+    <div className='login-container'>
+      <div className={`container ${action === "Login" ? "login-mode" : "signup-mode"}`}>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
 
+      <div className="inputs">
+        {action === "Signup" && (
+          <>
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+
+            <div className="input">
+              <img src={password_icon} alt="" />
+              <input type="password" placeholder="Password" />
+            </div>
+
+            <div className="input">
+              <img src={date_icon} alt="" />
+              <input type="text" placeholder="YYYY-MM-DD" />
+            </div>
+
+            <div className="input">
+              <img src={email_icon} alt="" />
+              <input type="email" placeholder="Email" />
+            </div>
+
+            <div className="input">
+              <img src={phone_icon} alt="" />
+              <input type="tel" placeholder="Phone Number" />
+            </div>
+
+            <div className="input">
+              <img src={location_icon} alt="" />
+              <input type="text" placeholder="Location" />
+            </div>
+          </>
+        )}
+
+        {action === "Login" && (
+          <>
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+
+            <div className="input">
+              <img src={password_icon} alt="" />
+              <input type="password" placeholder="Password" />
+            </div>
+          </>
+        )}
+
+        <div className="submit-container">
+          <div
+            className={action === "Signup" ? "submit gray" : "submit"}
+            onClick={() => setAction("Signup")}
+          >
+            Sign Up
+          </div>
+
+          <div
+            className={action === "Login" ? "submit gray" : "submit"}
+            onClick={() => setAction("Login")}
+          >
+            Login
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+    </div>
+  );
+};
 
 export default LoginSignup;
-
